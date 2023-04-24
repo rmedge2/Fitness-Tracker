f824645bf6f45fa2532ee2812ed35c0e0e74c37b
Shineka Solomon <neka.solomon@gmail.com>
2023-04-24 14:15:25 -0400

    corrected db name and updated config file

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+module.exports = 
+  
+  {
+  "development": {
+    "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
+      "password": null,
+        "database": "fitnesstrackerdb",
+          "host": "127.0.0.1",
+            "dialect": "postgres"
+  },
+  "test": {
+    "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
+      "password": null,
+        "database": "fitnesstrackerdb_test",
+          "host": "127.0.0.1",
+            "dialect": "postgres"
+  },
+  "production": {
+    "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
+      "password": null,
+        "database": "fitnesstrackerdb_production",
+          "host": "127.0.0.1",
+            "dialect": "postgres"
+  }
+
+}
