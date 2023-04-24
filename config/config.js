1bba30efa9610f469fe87d569eca9ef112dbbadb
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-04-24 14:20:06 -0400

    made some changes

--- config/config.js
+++ config/config.js
@@ -1,20 +1,22 @@
+require('dotenv').config()
+module.exports =
 {
   "development": {
-    "username": "process.env.DEVELOPMENT_DATABASE_USERS_CORCODING",
+    "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
     "password": null,
-    "database": "database_development",
+    "database": "process.env.fitnesstrackerdb",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "test": {
-    "username": "process.env.DEVELOPMENT_DATABASE_USERS_CORCODING",
+    "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": "process.env.DEVELOPMENT_DATABASE_USERS_CORCODING",
+    "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
