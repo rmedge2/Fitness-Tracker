2c8873b308b405845f9d801af4d969669af17102
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-04-24 14:48:07 -0400

    fixed config file issues

--- config/config.js
+++ config/config.js
@@ -1,27 +1,27 @@
 require('dotenv').config()
 
-module.exports = 
-  
-  {
+module.exports =
+
+{
   "development": {
-    "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
-      "password": null,
-        "database": "fitnesstrackerdb",
-          "host": "127.0.0.1",
-            "dialect": "postgres"
+    "username": process.env.DB_USER,
+    "password": null,
+    "database": process.env.DB_NAME,
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   },
   "test": {
     "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
-      "password": null,
-        "database": "fitnesstrackerdb_test",
-          "host": "127.0.0.1",
-            "dialect": "postgres"
+    "password": null,
+    "database": "fitnesstrackerdb_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   },
   "production": {
     "username": "process.env.fitnesstrackerdb_USERS_CORCODING",
-      "password": null,
-        "database": "fitnesstrackerdb_production",
-          "host": "127.0.0.1",
-            "dialect": "postgres"
+    "password": null,
+    "database": "fitnesstrackerdb_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   }
-
+}
